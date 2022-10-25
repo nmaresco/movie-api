8bb5c0e063b3e4df0a58756a990629398b54776b
nmaresco <109701914+nmaresco@users.noreply.github.com>
2022-10-25 08:33:37 -0700

    Fixed bugs

--- models.js
+++ models.js
@@ -25,7 +25,7 @@ let userSchema = mongoose.Schema({
 });
 
 userSchema.statics.hashPassword = (password) => {
-  return bcrypt.hashsync(password, 10);
+  return bcrypt.hashSync(password, 10);
 };
 
 userSchema.methods.validatePassword = function(password) {
--- server.js
+++ server.js
@@ -13,11 +13,12 @@ app.use(cors());
 let auth = require('./auth')(app);
 const passport = require('passport');
 require('./passport');
+require('dotenv').config()
 const uuid= require('uuid')
 app.use(bodyParser.json());
 app.use(morgan('common'));
 app.use(express.static('public'));
-mongoose.connect('mongodb://localhost:27017/myflixDB', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.DB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
 .then(()=>{console.log("mongoDB is running.")})
 .catch(e=>{console.log(e)});
 
@@ -42,7 +43,7 @@ app.get('/movies', (req, res) => {
   Email: String,
   Birthday: Date
 }*/
-app.post('/users'
+app.post('/users',
   [
     check('Username', 'Username is required').isLength({min: 5}),
     check('Username', 'Username contains non alphanumeric characters - not allowed.').isAlphanumeric(),
@@ -56,32 +57,32 @@ app.post('/users'
     if (!errors.isEmpty()) {
       return res.status(422).json({ errors: errors.array() });
     };
-
-  let hashedPassword = Users.hashPassword(req.body.Password);
-  Users.findOne({ Username: req.body.Username })
-    .then((user) => {
-      if (user) {
-        return res.status(400).send(req.body.Username + ' already exists');
-      } else {
-        Users
-          .create({
-            Username: req.body.Username,
-            Password: hashedPassword,
-            Email: req.body.Email,
-            Birthday: req.body.Birthday
+    let hashedPassword = Users.hashPassword(req.body.Password);
+    Users.findOne({ Username: req.body.Username })
+      .then((user) => {
+        if (user) {
+          return res.status(400).send(req.body.Username + ' already exists');
+        } else {
+          Users
+            .create({
+              Username: req.body.Username,
+              Password: hashedPassword,
+              Email: req.body.Email,
+              Birthday: req.body.Birthday
+            })
+            .then((user) =>{res.status(201).json(user) })
+          .catch((error) => {
+            console.error(error);
+            res.status(500).send('Error: ' + error);
           })
-          .then((user) =>{res.status(201).json(user) })
-        .catch((error) => {
-          console.error(error);
-          res.status(500).send('Error: ' + error);
-        })
-      }
-    })
-    .catch((error) => {
-      console.error(error);
-      res.status(500).send('Error: ' + error);
-    });
-};
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+        res.status(500).send('Error: ' + error);
+      });
+
+});
 
 // Get all users
 app.get('/users', passport.authenticate('jwt',{session: false}) ,(req, res) => {
